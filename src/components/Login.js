7a8886ed8fcb48277368d4989bfd753b5bb2f7a2
A191254 <atharvwani01@gmail.com>
2022-05-08 22:18:52 +0530

    done partially

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,62 @@
 import React, { useState } from 'react'
 import '../css/login.css'
+import axios from "axios"
+
+
 function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function login(){
-      console.log(email,password);
-      // let item = {email, password}
+  const login = async()=>{
+    try{
+        let res=await axios({
+           method: 'post',
+           url: 'http://13.233.39.88:4000/user/login',
+           headers: {
+               "Content-Type": "application/json",
+               "Accept": "application/json"
+           }, 
+           data: {
+               "phone": email,
+               "password": password
+           }
+         });
+        console.log(res);
+        if (res.data.type === "admin"){
+            console.log("admin is here !");
+            const e1=document.getElementById("phoneId");
+            const e2=document.getElementById("passId");
+            
+            e1.classList.add("invisible");
+            e2.classList.add("invisible");
+        }
+        else{
 
+            // const e1=document.getElementById("phoneId");
+            // const e2=document.getElementById("passId");
+            
+            // e1.classList.add("visible");
+            // e2.classList.add("visible");
+            // firstNameField.classList.add("invalid");
+
+            console.log("hacker is here !");
+        }
+    }
+    catch(error){
+            const e3=document.getElementById("phoneId");
+            const e4=document.getElementById("passId");
+            
+            e3.classList.add("visible");
+            e4.classList.add("visible");
+            console.log("Wrong Credentials admin !");
+
+            var e5 = document.getElementById("labelId");
+            e5.innerHTML = "hi"
+
+    }
   }
+  
 
   return (
     <div>
@@ -18,8 +65,9 @@ function Login() {
                 <div className="left"></div>
                 <div className="right">
                     <h2>ENTER DETAILS</h2>
-                    <input type="text" className="field" onChange={(e) => setEmail(e.target.value)} placeholder="Your Phone" />
-                    <input type="password" className="field" onChange={(e) => setPassword(e.target.value)} placeholder="Your Password" />
+                    <input id="phoneId" type="text" className="field" onChange={(e) => setEmail(e.target.value)} placeholder="Your Phone" required maxLength="10"/>
+                    <input id="passId" type="password" className="field" onChange={(e) => setPassword(e.target.value)} placeholder="Your Password" required />
+                    <label htmlFor="error" id = "labelId">yo</label>
                     <button className="btn" onClick={login}>LOGIN</button>
                 </div>
             </div>
